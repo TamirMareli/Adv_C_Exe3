6796cafa34101da277c64f0e1161c11bdea73279
TamirMareli <tqmirmareli4949@gmail.com>
2022-04-13 09:57:20 +0300

    חסר פונקציה אחת וMAIN

--- Adv_C_Exe3/Queue.c
+++ Adv_C_Exe3/Queue.c
@@ -1,6 +1,16 @@
 #include "Queue.h"
 #include <stdio.h>
 
+void initQueue(Queue* q);
+void destroyQueue(Queue* q);
+void enqueue(Queue* q, unsigned int data);
+unsigned int dequeue(Queue* q);
+int isEmptyQueue(const Queue* q);
+void rotateQueue(Queue* q);
+void cutAndReplace(Queue* q);
+void sortKidsFirst(Queue* q);
+void cutAndReplaceOdd(Queue* q, int count);
+void cutAndReplaceEven(Queue* q, int count);
 /***************** Queue ADT Implementation *****************/
 
 void initQueue(Queue* q)
@@ -29,7 +39,7 @@ void destroyQueue(Queue* q)
 void enqueue(Queue* q, unsigned int data)
 {
 	intNode* temp = (intNode*)malloc(sizeof(intNode));
-	if (temp == NULL) { printf("allocation faild\n"; exit(1)); }
+	if (temp == NULL) { printf("allocation faild\n"); exit(1); }
 	temp->data = data;
 	temp->next = NULL;
 	if (q->head == NULL) {// if the queue is entpy
@@ -48,10 +58,9 @@ unsigned int dequeue(Queue* q)
 		printf("Cannot dequeue an empty queue\n");
 		return;
 	}
-	unsigned int temp;
+	unsigned int temp= q->head->data;
 	intNode* ptr = q->head;
 	if (q->head->next) {//if have only one object
-		temp = q->head->data;
 		q->head = NULL;
 		q->tail = NULL;
 		free(ptr);
@@ -75,15 +84,89 @@ int isEmptyQueue(const Queue* q)
 
 void rotateQueue(Queue* q)
 {
+	if (q->head == NULL)
+		return;
+	if (q->head->next == NULL)
+		return;
 
+	Queue temp;
+	while (q->head->next != NULL) {
+		enqueue(&temp, dequeue(q));
+	}
+	while (temp.head != NULL) {
+		enqueue(q, dequeue(&temp));
+	}
 }
 
 void cutAndReplace(Queue* q)
 {
-	// add your code here
+	if (q == NULL)//if NULL
+		return;
+	if (q->head == NULL)//if empty
+		return;
+
+	Queue temp1;
+	int count = 0;
+	while (!isEmptyQueue(q)) {
+		enqueue(&temp1, dequeue(q));
+		count++;
+	}
+	while (!isEmptyQueue(&temp1)) {
+		enqueue(q, dequeue(&temp1));
+	}
+	if (count % 2 == 1) {
+		cutAndReplaceOdd(q, count);
+	}
+	else {
+		cutAndReplaceEven(q, count);
+	}
 }
+
 
 void sortKidsFirst(Queue* q)
 {
 	// add your code here
+}
+
+void cutAndReplaceOdd(Queue* q,int count)
+{
+	int sum = 0;
+	int data = 0;
+	Queue temp4;
+	for (int i = 0; i < count; i++) {
+		data = dequeue(q);
+		sum += data;
+		enqueue(&temp4, data);
+	}
+	sum = sum / count;
+	enqueue(&temp4, sum);
+	count++;
+	for (int i = 0; i < count; i++) {
+		enqueue(q, dequeue(&temp4));
+	}
+	cutAndReplaceEven(q, count);
+}
+
+void cutAndReplaceEven(Queue* q, int count)
+{
+	Queue temp2;
+	Queue* temp3 = (Queue*)malloc(sizeof(Queue));
+	if (temp3 == NULL) { printf("allocation faild\n"); exit(1); }
+	for (int i = 0; i < count / 2; i++) {
+		enqueue(&temp2, dequeue(q));
+	}
+	for (int i = 0; i < count / 2; i++) {
+		enqueue(temp3, dequeue(q));
+	}
+	for (int i = 0; i < count / 2; i++) {
+		cutAndReplace(temp3);
+	}
+	for (int i = 0; i < count / 2; i++) {
+		enqueue(q, dequeue(temp3));
+	}
+	for (int i = 0; i < count / 2; i++) {
+		enqueue(q, dequeue(&temp2));
+	}
+	free(temp3);
+
 }
